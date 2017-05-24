8533280d9f69f347fba883cb4ad7ea5c680fcfaf
Alvaro Maceda <alvaro@alvaromaceda.es>
2017-05-24 12:59:41 +0200

    3

--- 4-Inventario/dinamico.py
+++ 4-Inventario/dinamico.py
@@ -5,7 +5,7 @@ import sys
 if sys.argv[1] == '--list':
     print '''
     {
-        "aplicaciones": ["app1", "app2", "app3"],
+        "aplicaciones": ["app1", "ejemplo", "app3"],
         "bd": ["bd1", "bd2"]
     }
     '''.strip()
@@ -15,6 +15,6 @@ else:
         "ansible_host": "127.0.0.1",
         "ansible_port":"2200",
         "ansible_user": "ubuntu",
-        "ansible_ssh_private_key_file: "../.vagrant/machines/vagrant2/virtualbox/private_key"
+        "ansible_ssh_private_key_file": "../.vagrant/machines/vagrant2/virtualbox/private_key"
     }
     '''.strip()
